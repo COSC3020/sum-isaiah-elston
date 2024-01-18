d7bb4c3c5a954ada58dcf1bd598d90445b2bf78c
Isaiah Elston <156853632+isaiah-elston@users.noreply.github.com>
2024-01-18 13:10:28 -0700

    Revert "Feedback"

--- code.js
+++ code.js
@@ -1,11 +1,7 @@
 function sum(a) {
-    var sum = 0;
+    var sum = a[0];
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
 }
-
-let testArray = [5, 10, 15]
-
-console.log(sum(testArray))
